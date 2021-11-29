52fdcd96ec7e3248c4a71c41c47d4bae40d380be
raoulzalotay <77750901+Rinaldo420@users.noreply.github.com>
2021-11-29 14:05:16 +0100

    test

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Calculator
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+         static char[] Operators = new[]{ '+', '-', '*', '/' };
+         static int[] _numbers = new int[]{};
+        
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string line = Console.ReadLine();
+            string[] splits = line.Split(' ');
+
+            foreach (var split in splits)
+            {
+                if (int.TryParse(split, out var re))
+                {
+                    int num = int.Parse(split);
+                    _numbers = new int[] {num};
+
+                }else if (split.Length==1 && Operators.Contains(split.First()))
+                {
+                    char.Parse(split);
+                    
+                }else
+                {
+                    Console.WriteLine("Wrong Input");
+                }
+                
+            }
+            for(k = 1; )
+            Console.WriteLine(_numbers);
         }
     }
 }
